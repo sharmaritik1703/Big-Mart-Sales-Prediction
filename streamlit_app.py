b53dd51e02eb77d5562d367dc7b72dbb2e2080e5
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-07-10 21:10:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,72 +4,76 @@ import pickle
 import streamlit as st
 import time
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('Model.h5', 'rb'))
 
-st.header("Big Mart Sales Predictor")
+st.header("Big Mart Sales Prediction System")
 
 # <=== Columns ===>
 col1, col2, col3 = st.columns(3)
 col4, col5, col6 = st.columns(3)
-col7, col8 = st.columns(2)
+col7, col8, col9 = st.columns(3)
 
 # <=== Encoded Values for Features ===>
-item_types = {'Fruits and Vegetables':0, 'Snack Foods':1, 'Household':2,'Frozen Foods':3, 'Dairy':4, 'Canned':5, 
-              'Baking Goods':6, 'Health and Hygiene':7, 'Soft Drinks':8, 'Meat':9, 'Breads':10, 'Hard Drinks':11, 
-              'Others':12, 'Starchy Foods':13, 'Breakfast':14, 'Seafood':15}
+fat_encode = {'Low Fat': 0, 'Regular': 1, 'LF': 2, 'reg': 3, 'low fat': 4}
 
-fat_content = {'LF': 0, 'Low Fat': 1, 'Regular': 2, 'low fat':3, 'reg':4}
-outlet_ids = {'OUT013': 0, 'OUT018':1, 'OUT019':2, 'OUT027':3, 'OUT035':4, 'OUT046':5, 'OUT049':6}
-outlet_size = {"Small": 0, "Medium": 1, "High": 2}
-outlet_location = {"Tier-1": 0, "Tier-2": 1, 'Tier-3': 2}
-outlet_type = {"Grocery Store":0, "Supermarket Type1":1, "Supermarket Type2":2, "Supermarket Type3": 3}
+outlet_id = {'OUT027': 0, 'OUT013': 1, 'OUT049': 2, 'OUT046': 3, 'OUT035': 4, 
+             'OUT045': 5, 'OUT018': 6, 'OUT017': 7, 'OUT010': 8, 'OUT019': 9
+            }
 
-# === Column Names ===
-columns = ['Item Type', 'Item Weight', 'Item MRP','Fat Content', 'Outlet Identifier', 'Outlet Size',
-           'Outlet Location Type', 'Outlet Type']
+outlet_size = {'Small': 0, 'Medium': 1, 'High': 2}
+
+outlet_loc_type = {'Tier 1': 0, 'Tier 2': 1, 'Tier 3': 2}
+
+outlet_type = {'Supermarket Type1': 0, 'Grocery Store': 3, 'Supermarket Type3': 2, 'Supermarket Type2': 1}
+
+item_type = {
+    'Fruits and Vegetables': 0, 'Snack Foods': 1, 'Household': 2,
+    'Frozen Foods': 3, 'Dairy': 4, 'Canned': 5, 'Baking Goods': 6,
+    'Health and Hygiene': 7, 'Soft Drinks': 8, 'Meat': 9, 
+    'Breads': 10, 'Hard Drinks': 11, 'Others': 12,
+    'Starchy Foods': 13, 'Breakfast': 14,'Seafood': 15
+ }
 
 st.sidebar.title("Welcome to Big Mart Sales Predictor")
 st.sidebar.markdown("This web application predicts the overall sales based upon these feature values.")
 
-st.sidebar.dataframe(columns)
-
-values = np.zeros(shape=(1, 8))
+values = np.zeros(shape=(1, 10))
 
 with col1: 
-    item = st.selectbox(f"{columns[0]}", options=item_types.keys())
-    values[0, 0] = item_types[item]
+    values[0, 0]= st.slider(label="Item Weight", min_value=0.00, max_value=25.00)
 
 with col2: 
-    values[0, 1] = st.slider(label=f'{columns[1]}', min_value=0.0, max_value=21.35)
+    values[0, 1] = fat_encode[st.selectbox(label='Item Fat Content', options=fat_encode.keys())]
 
 with col3: 
-    values[0, 2] = st.slider(label=f"{columns[2]}", min_value=0.0, max_value=277.0)
+    values[0, 2] = st.slider(label="Item Visibility", min_value=0.00, max_value=0.20)
 
 with col4:
-    fat = st.selectbox(f"{columns[3]}", options=fat_content.keys())
-    values[0, 3] = fat_content[fat]
+    values[0, 3] = item_type[st.selectbox(label="Item Type", options=item_type.keys())]
 
 with col5:
-    id = st.selectbox(f"{columns[4]}", options=outlet_ids.keys())
-    values[0, 4] = outlet_ids[id]
+    values[0, 4] = st.slider("Item MRP", min_value=0.00, max_value=270.00)
 
 with col6:
-    size = st.selectbox(f"{columns[5]}", options=outlet_size.keys())
-    values[0, 5] = outlet_size[size]
+    values[0, 5] = outlet_id[st.selectbox(label="Outlet Identifier", options=outlet_id.keys())]
 
 with col7: 
-    location = st.selectbox(f"{columns[6]}", options=outlet_location.keys())
-    values[0, 6] = outlet_location[location]
+    values[0, 6] = st.slider(label='Outlet Establishment Year', min_value=1970, max_value=2010)
 
 with col8: 
-    outlet = st.selectbox(f"{columns[7]}", options=outlet_type.keys())
-    values[0, 7] = outlet_type[outlet]
+    values[0, 7] = outlet_size[st.selectbox(label="Outlet Size", options=outlet_size.keys())]
+
+with col9: 
+    values[0, 8] = outlet_loc_type[st.selectbox(label="Outlet Location Type", options=outlet_loc_type.keys())]
+
+values[0, 9] = outlet_type[st.selectbox(label="Outlet Type", options=outlet_type.keys())]
+
+st.divider()
 
 def predict():
-    st.divider()
     pred_value = model.predict(values)[0]
-    st.progress(value=0, text="Running...")
-    time.sleep(1)
-    st.subheader(f"Predicted Sales: {np.round(pred_value)}$")
+    with st.expander("Sales"):
+        st.progress(value=100)
+        st.subheader(f"Predicted Sales: {np.round(pred_value)}$")
 
-st.button(label='See Prediction', on_click=predict)
+st.button(label='See Sales', on_click=predict)
