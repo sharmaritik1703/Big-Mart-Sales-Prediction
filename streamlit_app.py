499fe186323340a67f083a06f6f3788dd5a9ed58
Ritik Sharma <63917915+sharmaritik1703@users.noreply.github.com>
2023-07-10 21:19:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,16 +38,26 @@ st.sidebar.title("Welcome to Big Mart Sales Predictor")
 st.sidebar.markdown("This web application predicts the overall sales based upon these feature values.")
 
 st.sidebar.markdown("""
-Item_Weight ---- Weight of product
-Item_Fat_Content ---- Whether the product is low fat or not
-Item_Visibility ---- The % of the total display area of all products in a store allocated to the particular product
-Item_Type ---- The category to which the product belongs
-Item_MRP ---- Maximum Retail Price (list price) of the product
-Outlet_Identifier ---- Unique store ID
-Outlet_Establishment_Year ---- The year in which the store was established
-Outlet_Size ---- The size of the store in terms of ground area covered
-Outlet_Location_Type ---- The type of city in which the store is located
-Outlet_Type ---- Whether the outlet is just a grocery store or some sort of supermarket
+`Item_Weight` ---- Weight of product
+                    
+`Item_Fat_Content` ---- Whether the product is low fat or not
+                    
+`Item_Visibility` ---- Percentage the total display area of all products in a store allocated 
+to the particular product
+                    
+`Item_Type` ---- The category to which the product belongs
+                    
+`Item_MRP` ---- Maximum Retail Price (list price) of the product
+                    
+`Outlet_Identifier` ---- Unique store ID
+                    
+`Outlet_Establishment_Year` ---- The year in which the store was established
+                    
+`Outlet_Size` ---- The size of the store in terms of ground area covered
+                    
+`Outlet_Location_Type` ---- The type of city in which the store is located
+                    
+`Outlet_Type` ---- Whether the outlet is just a grocery store or some sort of supermarket
 """)
 
 values = np.zeros(shape=(1, 10))
